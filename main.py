3b28daa3ea1def55a8ae7a9b3646ba1535c035c8
Tariq <qwerty2945@outlook.com>
2021-02-18 23:23:10 -0500

    prototype script

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import subprocess
 import os
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # subprocess.run("")
-
-    os.system('ls')
+    DATE = '12/16/20'
+    os.system('touch test')
+    os.system('git add test')
+    os.system(f'git commit -m test --date {DATE}')
+    os.system('git push')
