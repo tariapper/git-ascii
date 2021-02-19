3ae078a957f9d1ecdd86dc779d86ff91749fd401
Tariq <qwerty2945@outlook.com>
2021-02-19 16:52:34 -0500

    Bugfixes and optimizations to parse_input and git push

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # git-ascii
 
+python3 main.py -i inputs/hello.txt
+
 to revert - 
 
 git reset 2c08fa882d10600756ca765fc4bb492d0687fe5a
 
 git push origin master -f
+
+https://www.ascii-art-generator.org/
+
+53x7
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import subprocess
 import os
 import argparse
 from datetime import date, timedelta
+from itertools import zip_longest
 
-WEEKLENGTH = 7
+WEEK_LENGTH = 7
 TODAY = date.today()
 PARSED_INPUT = []
+COMMIT_FILE_NAME = "art"
 
 
 def get_program_arguments():
@@ -24,27 +26,21 @@ def parse_input(file):
     with open(file) as f:
         counter = 0
         for row in f:
-            if counter < WEEKLENGTH:  # only parse first 7 rows of text file
+            if counter < WEEK_LENGTH:
                 counter += 1
-                current_row = []
-                for letter in row:
-                    if letter != '\n':
-                        current_row.append(letter)
-                PARSED_INPUT.append(current_row)
-    return list(map(list, zip(
-        *PARSED_INPUT)))  # https://www.geeksforgeeks.org/python-transpose-elements-of-two-dimensional-list/
+                PARSED_INPUT.append(list(row[:-1]))
+    return [list(filter(None, i)) for i in zip_longest(*PARSED_INPUT)]
 
 
 def get_sunday(n_weeks):
-    # TODO
+    # TODO convert input to date obj if custom start date
     if n_weeks == 'min':
-        n_weeks = len(PARSED_INPUT)
+        n_weeks = len(PARSED_INPUT) - 1  # untested
     if n_weeks == 'max':
         n_weeks = 51
     try:
         print(n_weeks)
-        return TODAY - timedelta(TODAY.weekday() + (1 + 7 * n_weeks))  # .strftime('%Y.%m.%d')
-        # n = 0 is current week sunday
+        return TODAY - timedelta(TODAY.weekday() + (1 + 7 * n_weeks))  # n = 0 is current week sunday
     except:
         raise Exception(f"Date ({args.start}) input formatted incorrectly.")
 
@@ -57,28 +53,29 @@ def send_commits(send_date):
     for column in PARSED_INPUT:
         for char in column:
             if char != ' ':
-                # git_push(send_date.strftime('%Y.%m.%d'))
-                print(send_date.strftime('%Y.%m.%d'))
+                git_push(send_date.strftime('%Y.%m.%d'))
             send_date += timedelta(1)
         send_date += timedelta(6 - send_date.weekday() % 7)  # no need to check if its already sunday, only progresses if not sunday
 
 
 def git_push(commit_date):
+    # creating file only necessary for first time, move to different function
     if os.name == 'posix':
-        os.system('touch test')
+        os.system(f'touch {COMMIT_FILE_NAME}')
     elif os.name == 'nt':
-        os.system('New-Item test')
+        os.system(f'New-Item {COMMIT_FILE_NAME}')  # untested, won't work if using cmd
     else:
         raise Exception('Could not detect platform information.')
 
-    os.system('git add test')
-    os.system(f'git commit -m test --date {commit_date} --allow-empty')
-    os.system('git push origin master --force')
+    os.system(f'git add {COMMIT_FILE_NAME}')
+    os.system(f'git commit -m {COMMIT_FILE_NAME} --date {commit_date} --allow-empty')
 
 
 if __name__ == '__main__':
     args = get_program_arguments()
-    print(args)
+    # print(args)
     PARSED_INPUT = parse_input(args.input)
-    print(PARSED_INPUT)
+    print(len(PARSED_INPUT))
     send_commits(get_sunday(args.start))
+    os.system('git pull')
+    os.system('git push origin master --force')
