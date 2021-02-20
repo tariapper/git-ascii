8dab28bf7cbd5e2f8d69f2d28b59777c80415d26
Tariq <tariapper@users.noreply.github.com>
2021-02-20 15:37:32 -0500

    Commented out code creating an empty file

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def get_program_arguments():
 
     parser.add_argument('--start', metavar='s', type=str, help='', default='min')
     # YYYY.MM.DD, MM/DD/YYYY or DD.MM.YYYY
-    # Specify top-left position of art, by weeks (0 - 52) default tight fit to right side
-
+    # Specify top-left position of art, by weeks (0 - 52) default tight fit to left side
+    # TODO allow more precision in placement
     return parser.parse_args()
 
 
@@ -28,18 +28,18 @@ def parse_input(file):
         for row in f:
             if counter < WEEK_LENGTH:
                 counter += 1
-                PARSED_INPUT.append(list(row[:-1]))
+                PARSED_INPUT.append(list(row[:-1]))  # exclude \n line ending
+                # TODO max of 53 char
     return [list(filter(None, i)) for i in zip_longest(*PARSED_INPUT)]
 
 
 def get_sunday(n_weeks):
-    # TODO convert input to date obj if custom start date
     if n_weeks == 'min':
         n_weeks = len(PARSED_INPUT) - 1  # untested
     if n_weeks == 'max':
-        n_weeks = 51
+        n_weeks = 52
     try:
-        print(n_weeks)
+        n_weeks = int(n_weeks)
         return TODAY - timedelta(TODAY.weekday() + (1 + 7 * n_weeks))  # n = 0 is current week sunday
     except:
         raise Exception(f"Date ({args.start}) input formatted incorrectly.")
@@ -53,29 +53,37 @@ def send_commits(send_date):
     for column in PARSED_INPUT:
         for char in column:
             if char != ' ':
-                git_push(send_date.strftime('%Y.%m.%d'))
+                # print(send_date.strftime('%Y.%m.%d'))
+                git_commit(send_date.strftime('%Y.%m.%d'))
             send_date += timedelta(1)
-        send_date += timedelta(6 - send_date.weekday() % 7)  # no need to check if its already sunday, only progresses if not sunday
+        send_date += timedelta(6 - send_date.weekday() % 7)
+        # no need to check if its already sunday, only progresses if not sunday
 
 
-def git_push(commit_date):
-    # creating file only necessary for first time, move to different function
+def initalize():
     if os.name == 'posix':
         os.system(f'touch {COMMIT_FILE_NAME}')
     elif os.name == 'nt':
-        os.system(f'New-Item {COMMIT_FILE_NAME}')  # untested, won't work if using cmd
+        inp = input("Windows platform detected. Are you using cmd or powershell? Press 1 for cmd or 2 for powershell:")
+        if inp == "1":
+            os.system(f'NUL>{COMMIT_FILE_NAME}')
+        elif inp == "2":
+            os.system(f'New-Item -path \"./\" -type file -name \"{COMMIT_FILE_NAME}\"')
+        else:
+            raise Exception('Invalid number entered.')
     else:
         raise Exception('Could not detect platform information.')
 
-    os.system(f'git add {COMMIT_FILE_NAME}')
+
+def git_commit(commit_date):
+    # os.system(f'git add {COMMIT_FILE_NAME}')
     os.system(f'git commit -m {COMMIT_FILE_NAME} --date {commit_date} --allow-empty')
 
 
 if __name__ == '__main__':
     args = get_program_arguments()
-    # print(args)
+    # initalize() -- creating a file not necessary if git allows empty commits
     PARSED_INPUT = parse_input(args.input)
-    print(len(PARSED_INPUT))
     send_commits(get_sunday(args.start))
     os.system('git pull')
     os.system('git push origin master --force')
